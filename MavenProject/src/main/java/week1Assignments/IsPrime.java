969841239afa6aa174cf5cee4d75920acc9d741b
Mounika821 <125535189+Mounika821@users.noreply.github.com>
2024-12-12 23:15:44 +0530

    Reapply "FirstProject"
    This reverts commit e6d19f64659e136fb741f73c0e7479d87299b20f.

---
+++ MavenProject/src/main/java/week1Assignments/IsPrime.java
@@ -0,0 +1,21 @@
+package week1Assignments;
+
+import java.util.Scanner;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+		        int num = 4;
+		        if (isPrime(num)) {
+		            System.out.println(num + " is prime");
+		        }
+		        else {
+		            System.out.println(num + " is not prime");
+		        }
+	}
+
+	private static boolean isPrime(int num) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
